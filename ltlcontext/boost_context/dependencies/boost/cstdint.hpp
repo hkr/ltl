a6f74a5c1d71bad6cd25a9ea974b60c3acae5521
Florian Penzkofer <fp@nullptr.de>
2014-03-02 14:18:12 +0100

    Initial boost.context integration

---
+++ ltlcontext/boost_context/dependencies/boost/cstdint.hpp
@@ -0,0 +1,14 @@
+#ifndef LTLCONTEXT_BOOST_CONTEXT_DEPENDENCIES_CSTDINT_HPP
+#define LTLCONTEXT_BOOST_CONTEXT_DEPENDENCIES_CSTDINT_HPP
+
+#include <cstdint>
+
+namespace boost {
+    
+typedef std::uint32_t uint32_t;
+typedef std::uint64_t uint64_t;
+    
+} // namespace boost
+
+
+#endif // LTLCONTEXT_BOOST_CONTEXT_DEPENDENCIES_CSTDINT_HPP
