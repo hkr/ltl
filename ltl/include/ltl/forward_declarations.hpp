a91eaa644099ce7ca5e8368d1b6c46e3f69bd2e7
Florian Penzkofer <fp@nullptr.de>
2014-03-04 20:48:22 +0100

    Added header with forward declarations

---
+++ ltl/include/ltl/forward_declarations.hpp
@@ -0,0 +1,12 @@
+#ifndef LTL_FORWARD_DECLARATIONS_HPP
+#define LTL_FORWARD_DECLARATIONS_HPP
+
+namespace ltl {
+    
+template <typename T> class future;
+template <typename T> class promise;
+class task_queue;
+    
+} // namespace ltl
+
+#endif /// LTL_FORWARD_DECLARATIONS_HPP
