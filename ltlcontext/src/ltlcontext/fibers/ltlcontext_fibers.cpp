ee724c7b14808391e60c0af469e6c2579f6c9649
Florian Penzkofer <fp@nullptr.de>
2014-03-05 21:44:24 +0100

    Adapted Fibers implementation to new context abstraction interface.

--- ltlcontext/src/ltlcontext/fibers/ltlcontext_fibers.cpp
+++ ltlcontext/src/ltlcontext/fibers/ltlcontext_fibers.cpp
@@ -6,57 +6,32 @@
 
 #include <new>
 
-#define LTL_ASSERT(cond) do { if (!cond) DebugBreak(); } while (false)
-
 namespace ltl {
-    
-
-
-struct context
-{
-	explicit context(void* fiber) : value(fiber)
-    {
-    }
-    void* value;
-};
-    
+        
 void jump(context*, context* nfc)
 {
-	LTL_ASSERT(nfc);
-	LTL_ASSERT(nfc->value);
-	SwitchToFiber(nfc->value);
+	assert(nfc);
+	SwitchToFiber(nfc);
 }
 
-context* create_context(void* stack, std::size_t size, void (*fn)(context_data_t), context_data_t vp)
+context* create_context(std::size_t stack_size, void (*fn)(context_data_t), context_data_t vp)
 {
-    if (size < sizeof(context))
-        return nullptr;
+	return static_cast<context*>(CreateFiber(stack_size, reinterpret_cast<LPFIBER_START_ROUTINE>(fn), reinterpret_cast<LPVOID>(vp)));
+}
 
-	// TODO: rework how to allocate stack, create_context interface does not work well with Fibers
-	// TODO: pass allocator function instead
-    
-	// casting function pointer types is ugly, but undefined bahavior, but this is platform-dependent anyway
-	auto fiber = CreateFiber(0, reinterpret_cast<LPFIBER_START_ROUTINE>(fn), reinterpret_cast<LPVOID>(vp));
-	if (!fiber)
-	{
-		DWORD err = GetLastError();
-		int i = 0;
-		++i;
-	}
-	LTL_ASSERT(fiber);
-	return new (stack)context(fiber);
+void destroy_context(context* ctx)
+{
+	DeleteFiber(ctx);
 }
 
 context* create_main_context()
 {
-	auto fiber = ConvertThreadToFiber(nullptr);
-	LTL_ASSERT(fiber);
-	return new context(fiber);
+	return static_cast<context*>(ConvertThreadToFiber(nullptr));
 }
 
-void destroy_main_context(context* ctx)
+void destroy_main_context(context*)
 {
-    delete ctx;
+	ConvertFiberToThread();
 }
     
 } // namespace ltl
