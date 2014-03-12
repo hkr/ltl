cd9916d684c99983390dfd43ce5dacc641da2f26
Florian Penzkofer <fp@nullptr.de>
2014-03-12 10:05:35 +0100

    Implemented iomanager::connect and socket::read

--- lio/src/lio/iomanager.cpp
+++ lio/src/lio/iomanager.cpp
@@ -39,6 +39,7 @@ struct iomanager::impl
     impl(impl const&) = delete;
     impl& operator=(impl const&) = delete;
     
+    
     explicit impl(iomanager* ptr)
     : back_(ptr)
     , loop_(make_loop())
@@ -55,15 +56,35 @@ struct iomanager::impl
         return server(back_->shared_from_this(), ip, port, std::move(on_connection));
     }
     
+    struct connection_request
+    {
+        connection_request(connection_request const&) =delete;
+        connection_request& operator=(connection_request const&) =delete;
+        explicit connection_request(uv_loop_t* loop)
+        : prm()
+        , conn()
+        , sock(std::make_shared<uv_tcp_t>())
+        {
+            uv_tcp_init(loop, sock.get());
+            conn.data = this;
+        }
+        ltl::promise<std::shared_ptr<socket>> prm;
+        uv_connect_t conn;
+        std::shared_ptr<uv_tcp_t> sock;
+    };
+    
     ltl::future<std::shared_ptr<socket>> connect(char const* ip, int port)
     {
-        
-        return ltl::make_future(std::shared_ptr<socket>());
+        auto req = new connection_request(loop_.get());
+        uv_tcp_connect(&req->conn, req->sock.get(), uv_ip4_addr(ip, port), connection_established);
+        return req->prm.get_future();
     }
     
-    static void connection_established()
+    static void connection_established(uv_connect_t* conn, int status)
     {
-        
+        auto req = static_cast<connection_request*>(conn->data);
+        req->prm.set_value(std::make_shared<socket>(req->sock));
+        delete req;
     }
     
     std::shared_ptr<uv_loop_s> get_loop()
--- lio/src/lio/socket.cpp
+++ lio/src/lio/socket.cpp
@@ -10,20 +10,65 @@ namespace lio {
 struct socket::impl
 {
     explicit impl(std::shared_ptr<uv_tcp_s> const& tcp)
-    : loop_(static_cast<iomanager*>(tcp->loop->data)->get_loop())
+    : in_progress_(false)
+    , read_promise_()
+    , read_buf_()
+    , loop_(static_cast<iomanager*>(tcp->loop->data)->get_loop())
     , tcp_(tcp)
+    {
+        tcp_->data = this;
+    }
+    
+    static uv_buf_t alloc_read(uv_handle_t* handle, size_t suggested_size)
+    {
+        return static_cast<impl*>(handle->data)->on_alloc_read(handle, suggested_size);
+    }
+    
+    uv_buf_t on_alloc_read(uv_handle_t* handle, size_t suggested_size)
+    {
+        uv_buf_t buf;
+        buf.len = read_buf_.size();
+        buf.base = reinterpret_cast<char*>(read_buf_.data());
+        return buf;
+    }
+    
+    static void read_cb(uv_stream_t* stream, ssize_t nread, uv_buf_t buf)
+    {
+        static_cast<impl*>(stream->data)->on_read(stream, nread, buf);
+    }
+    
+    static void write_cb(uv_write_t* req, int status)
     {
         
     }
     
-    ltl::future<void> write(void const* data, std::size_t size)
+    void on_read(uv_stream_t*, ssize_t nread, uv_buf_t)
     {
-        return ltl::make_future();
+        in_progress_ = false;
+        read_promise_.set_value(std::move(read_buf_));
     }
     
     ltl::future<std::vector<uint8_t>> read(std::size_t size)
     {
-        return ltl::make_future(std::vector<uint8_t>());
+        if (in_progress_)
+            return ltl::future<std::vector<uint8_t>>();
+        
+        in_progress_ = true;
+        
+        ltl::promise<std::vector<std::uint8_t>> new_read_promise;
+        std::vector<std::uint8_t> new_read_buf(size);
+        read_promise_.swap(new_read_promise);
+        read_buf_.swap(new_read_buf);
+        
+        uv_read_start((uv_stream_t*)tcp_.get(), alloc_read, read_cb);
+        
+        return read_promise_.get_future();
+    }
+    
+    ltl::future<void> write(void const* data, std::size_t size)
+    {
+        
+        return ltl::make_ready_future();
     }
     
     void close()
@@ -34,6 +79,9 @@ struct socket::impl
     impl(impl const&) = delete;
     impl& operator=(impl const&) = delete;
     
+    bool in_progress_;
+    ltl::promise<std::vector<std::uint8_t>> read_promise_;
+    std::vector<std::uint8_t> read_buf_;
     std::shared_ptr<uv_loop_t> loop_;
     std::shared_ptr<uv_tcp_t> tcp_;
 };
